e5931481e471121944adcc10edc2d2292068b2ae
Pedro Augusto de Portilho Ronzani <pedroaugustogabironzani@hotmail.com>
2023-03-17 10:37:17 -0300

    Reduzindo template

--- Util/temp.cpp
+++ Util/temp.cpp
@@ -5,43 +5,38 @@ using namespace std;
 #define _ ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 #define int            long long int
-#define vi             vector <int>
-#define endl           "\n"
 #define double         long double
+#define endl           "\n"
 #define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
-#define print(a)       for(auto x : a) cout << x << " "; cout << endl
-#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
+#define print_v(a)     for(auto x : a) cout << x << " "; cout << endl
+#define print_vp(a)    for(auto x : a) cout << x.F << " " << x.S << endl
 #define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
 #define f(i,s,e) 	   for(int i=s;i<e;i++)
 #define rf(i,e,s) 	   for(int i=e-1;i>=s;i--)
 
 const int INF = 0x7f3f3f3f;
-const int MAX = 1e6+10; // 10⁶ + 10
+const int MAX = 1e8+10; // 10⁶ + 10
 
-vector<int> vec(MAX, 0);
-
-template <typename Arg1>
-void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
-template <typename Arg1, typename... Args>
-void __f (const char* names, Arg1&& arg1, Args&&... args) {
+template <typename Arg1> void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args> void __f (const char* names, Arg1&& arg1, Args&&... args) {
 	const char* comma = strchr (names + 1, ',');
 	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
 }
 
 void solve() {
-	int n, m;
-	cin >> n >> m;
-	bug(n,m);
+	
 }
 
 int32_t main() { _
 	
 	clock_t z = clock();
 
+
 	int t = 1; // cin >> t;
 	while (t--) solve();
 
-	cerr << fixed << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << endl;
 
+	cerr << fixed << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << endl;
 	return 0;
 }
+
