8cb76f80788ab6cf2cf166a8901f13acc4d932b9
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2024-01-28 00:28:54 -0300

    Material

---
+++ Biblioteca/Materiais/Grafos/euler_tree.cpp
@@ -0,0 +1,27 @@
+vector<vector<int>> adj(MAX);
+vector<int> vis(MAX, 0);
+vector<int> euTree(MAX);
+
+void eulerTree(int u, int &index)
+{
+    vis[u] = 1;
+    euTree[index++] = u;
+    for (auto it : adj[u]) {
+        if (!vis[it]) {
+            eulerTree(it, index);
+            euTree[index++] = u;
+        }
+    }
+}
+
+int main() {
+
+	f(i,0,n-1) {
+		int a, b; cin >> a >> b;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+
+    int index = 0;
+	eulerTree(1, index);
+}
