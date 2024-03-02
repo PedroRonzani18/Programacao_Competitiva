9737bcea7300a99cab5f6d6fcde6cb2c77e50688
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2024-03-02 13:46:02 -0300

    Bee

--- Treinamentos/Beecrowd/bee3057.cpp
+++ Treinamentos/Beecrowd/bee3057.cpp
@@ -21,120 +21,101 @@ using namespace std;
 
 const int INF =  0x7f3f3f3f; // 0x7f com 3 3f's (10^9)
 const int LINF = 0x3f3f3f3f3f3f3f3f; // 0x com 7 3f's (10^18)
-const int MAXN = 1e5+10; // 10^6 + 10
+const int MAX = 1e6+10; // 10^6 + 10
 
 vector<vector<int>> adj;
-set<int> verdades;
-unordered_set<int> possivelmenteVerdades;
-vector<unordered_set<int>> asLigadosAoB;
+unordered_set<int> axiomas;
+vector<unordered_set<int>> fullPath;
+vector<bool> ansVec;
 
-int dfs(int v, int verdadePai) {
+void dfs(int v) {
 
+	if(!fullPath[v].empty()) 
+		return;
 
-	cout << "\nVerdades: "; print_v(verdades);
-	cout << "Current: " << v << " | Pai: " << verdadePai << endl;
-
-	// se ele ja for verdade, ou for uma folha
-	if(!asLigadosAoB[v].empty() and adj[v].empty()) {
-		if(verdadePai == 1) verdades.insert(v); // se seu pai for completamente verdadeiro, eu sou verdadeiro 
-		cout << "Folha: " << v << " -> " << verdades.count(v) << endl;
-		return verdades.count(v);
+	for(auto u : adj[v]) {
+		dfs(u);
+		fullPath[v].insert(all(fullPath[u]));
+		fullPath[v].insert(u);
 	}
-		
-	// se todos os pais forem verdadeiros, eu sou verdadeiro
-	if(!asLigadosAoB[v].empty()) {
-		bool paisVerdadeiros = true;
-		for(auto a : asLigadosAoB[v]) {
-			if (!verdades.count(a)) {
-				paisVerdadeiros = false;
-				break;
+}
+
+bool allContainsTarget(int target, vector<unordered_set<int>*>& sets) {
+	for(auto s : sets) 
+		if(!s->count(target))
+			return false;
+	return true;
+}
+
+void eventoIsTrue(int statement) {
+
+	vector<unordered_set<int>*> axiomasComVerdade;
+
+	for(auto axiom : axiomas) 
+		if(fullPath[axiom].count(statement))
+			axiomasComVerdade.push_back(&fullPath[axiom]);
+
+	for(auto implicacao : fullPath[statement])
+		ansVec[implicacao] = true;
+
+	for(int i = 0; i<fullPath.size(); i++)
+		if(!ansVec[i] and allContainsTarget(i, axiomasComVerdade))
+			ansVec[i] = true;
+}
+
+set<int> vecBoolToSet() {
+	set<int> s;
+	f(i,0,ansVec.size()) {
+		if(ansVec[i]) s.insert(i);
+	}
+	return s;
+}
+
+void printSet(const set<int>& set) {
+
+	if(!set.empty()) {
+		cout << *set.begin() + 1;
+
+		if(set.size() > 1) {
+			for(auto it = next(set.begin()); it != set.end(); it++)  {
+				cout << " " << *it + 1;
 			}
 		}
-		if (paisVerdadeiros) {
-			verdades.insert(v);
-			cout << "todos meus "<<asLigadosAoB[v].size()<<" pais sao verdadeiros" << endl;
-		}
+
+		cout << endl;
 	}
-
-	if(verdades.count(v) and adj[v].empty()) {
-		int quantidadeDePais = asLigadosAoB[v].size();
-		dbgl(quantidadeDePais);
-		if(quantidadeDePais > 1) return 0; // sou marromeno verdadeiro
-		return 1; // sou verdadeiro ou sou um axioma
-	}
-
-	int unclearCount = 0, falseCount = 0, trueCount = 0;
-
-    for (int u : adj[v]) {
-		int ret = dfs(u, (verdades.count(v) || verdadePai == 1) ? 1 : -1);
-		if(ret == -1) falseCount++;
-		else if(ret == 1) trueCount++;
-		else unclearCount++;
-    }
-
-	cout << "\nVerdades: "; print_v(verdades);
-	cout << "Current: " << v << endl;
-	cout << "Unclear: " << unclearCount << " False: " << falseCount << " True: " << trueCount << endl;
-
-	int filhos = adj[v].size();
-
-	// se sou um intermadiario
-	if(asLigadosAoB[v].size()) {
-		if(trueCount + unclearCount == filhos) {
-			possivelmenteVerdades.insert(v);
-			return 1;
-		}
-	} else { // se sou um axioma	
-		if(unclearCount + trueCount == filhos) {
-			verdades.insert(v);
-			possivelmenteVerdades.erase(v);
-			return 0;
-		}
-
-		possivelmenteVerdades.erase(v);
-		return -1;
-	}
-
-	return 0;
 }
 
 void solve() {
 
-	int e, imp, v; cin >> e >> imp >> v;
-	
-	adj.resize(e+1);
-	asLigadosAoB.resize(e+1);
+	int n, edg, verdades; cin >> n >> edg >> verdades;
 
-	f(i,0,imp) {
-		int a, b; cin >> a >> b;
+	adj.resize(n);
+	fullPath.resize(n);
+
+	f(i,0,n) {
+		axiomas.insert(i);
+	}
+
+	f(i,0,edg) {
+		int a, b; cin >> a >> b; a--, b--;
 		adj[a].push_back(b);
-		asLigadosAoB[b].insert(a);
+		axiomas.erase(b);
 	}
 
-	f(i,0,v) {
-		int verdade; cin >> verdade;
-		verdades.insert(verdade);
+	for(auto axioma : axiomas) {
+		dfs(axioma);
+		fullPath[axioma].insert(axioma);
 	}
 
-	unordered_set<int> axiomas;
-	f(i,1,e+1) {
-		if (asLigadosAoB[i].empty()) {
-			axiomas.insert(i);
-		}
+	ansVec.resize(n, false);
+
+	f(i,0,verdades) {
+		int verdade; cin >> verdade; verdade--;
+		eventoIsTrue(verdade);
 	}
 
-	for(auto x : axiomas) {
-		cout << "\n------------- Ponta: " << x << "---------------------------" << endl;
-		dfs(x, verdades.count(x) ? 1 : -1);
-
-	}
-
-	cout << *verdades.begin();
-
-	for(auto it = ++verdades.begin(); it != verdades.end(); it++) {
-		cout << " " << *it;
-	}
-	cout << endl;
+	printSet(vecBoolToSet());
 }
 
 int32_t main() { _
