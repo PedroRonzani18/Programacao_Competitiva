c367ef8d997715edd759223d8d7518487f02adf5
zulisses <carvalhoandradeulisses@gmail.com>
2024-03-09 10:58:44 -0300

    ajuste_mmc

--- Biblioteca/Materiais/Matematica/mmc_multiplo.cpp
+++ Biblioteca/Materiais/Matematica/mmc_multiplo.cpp
@@ -1,9 +1,10 @@
 // Description: Calcula o mmc de um vetor de inteiros.
 // Complexidade: O(nlogn) onde n eh o tamanho do vetor
-int mmc_many(vector<int> arr) {
-   int result = arr[0];
-   
-   for(int& num : arr)
-       result = (num * result / mmc(num, result ));
-   return ans;
+int mmc_many(vector<int> arr)
+{
+    int result = arr[0];
+
+    for (int &num : arr)
+        result = (num * result / mdc(num, result));
+    return result;
 }
