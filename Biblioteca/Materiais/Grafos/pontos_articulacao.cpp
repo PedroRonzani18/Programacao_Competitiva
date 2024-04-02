103e03740fa316b54ab76c5413e844777c510d09
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2024-04-02 16:00:45 -0300

    Articulacao

--- Biblioteca/Materiais/Grafos/pontos_articulacao.cpp
+++ Biblioteca/Materiais/Grafos/pontos_articulacao.cpp
@@ -1,66 +1,53 @@
-// Description: Encontra os pontos de articulação e pontes de um grafo não direcionado
-// Complexidade: O(n + m)
+// Description: Encontra os pontos de articulação de um grafo não direcionado
+// Complexidade: O(V*(V+E))
 
-vector<vector<pair<int,int>>> adj;
-vector<int> dfs_num, dfs_low, dfs_parent, articulation_vertex;
-int dfsCounter, dfsRoot, rootChildren;
+int V;
+vector<vi> adj;
+vi ans;
 
-vector<int> verticesAns;
-vector<pair<int,int>> bridgesAns;
-
-void articulationPointAndBridge(int u) {
-    dfs_low[u] = dfs_num[u] = dfsCounter++;
-    for (auto &[v, w] : adj[u]) {
-        if (dfs_num[v] == -1) {
-            dfs_parent[v] = u;
-            if (u == dfsRoot) ++rootChildren;
-
-            articulationPointAndBridge(v);
-
-            if (dfs_low[v] >= dfs_num[u])
-                articulation_vertex[u] = 1;
-            if (dfs_low[v] > dfs_num[u]) {
-                bridgesAns.emplace_back(u, v);    
-            }
-            dfs_low[u] = min(dfs_low[u], dfs_low[v]);
-        }
-        else if (v != dfs_parent[u])
-            dfs_low[u] = min(dfs_low[u], dfs_num[v]);
-    }
+void dfs(vector<bool>& vis, int i, int curr) {
+	vis[curr] = 1;
+	for (auto x : adj[curr]) {
+		if (x != i) {
+			if (!vis[x]) {
+				dfs(vis, i, x);
+			}
+		}
+	}
 }
 
-void findArtBridges(int n) {
+void AP() {
 
-    for (int u = 0; u < n; ++u) {
-        if (dfs_num[u] == -1) {
-            dfsRoot = u; rootChildren = 0;
-            articulationPointAndBridge(u);
-            articulation_vertex[dfsRoot] = (rootChildren > 1);
-        }
-    }
-
-    for (int u = 0; u < n; ++u) {
-        if (articulation_vertex[u]) {
-            verticesAns.push_back(u);
-        }
-    }
+    f(i,1,V+1) {
+		int components = 0;
+		vector<bool> vis(V + 1, 0);
+        f(j,1, V+1) {
+			if (j != i) {
+				if (!vis[j]) {
+					components++;
+					dfs(vis, i, j);
+				}
+			}
+		}
+		if (components > 1) {
+			ans.push_back(i);
+		}
+	}
 }
 
 void solve() {
 
-    int n, edg; cin >> n >> edg;
-    adj.assign(n, vector<pair<int,int>>());
-    while(edg--) {
-        int a, b, w; cin >> a >> b >> w;
-        adj[a].emplace_back(b, w);
-    }
+	V = n; 
+	adj.clear(), ans.clear(); 
+    adj.resize(V+1);
+    
+	while(edg--) {
+		int a, b; cin >> a >> b;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
 
-    dfs_num.assign(n, -1); dfs_parent.assign(n, -1);
-    dfs_low.assign(n,  0); articulation_vertex.assign(n, 0);
-    dfsCounter = 0;
+	AP();
 
-    findArtBridges(n);
-
-    // Vertices: verticesAns
-    // Bridges: bridgesAns
+	// Vertices articulacao: ans
 }
