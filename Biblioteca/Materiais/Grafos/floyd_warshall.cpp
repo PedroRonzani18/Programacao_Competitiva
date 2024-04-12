159bcc4511d4c9613991daa5fc66aa09b73d040d
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2024-04-12 11:10:54 -0300

    Floyd Warshall

--- Biblioteca/Materiais/Grafos/floyd_warshall.cpp
+++ Biblioteca/Materiais/Grafos/floyd_warshall.cpp
@@ -3,20 +3,49 @@
 
 const int INF = 1e9;
 const int MAX_V = 450;
-int AM[MAX_V][MAX_V];
+int adj[MAX_V][MAX_V];
+
+void printAnswer(int n) {
+	for (int u = 0; u < n; ++u)
+	for (int v = 0; v < n; ++v)
+	cout << "APSP("<<u<<", "<<v<<") = " << adj[u][v] << endl;
+}
+
+void prepareParent() {
+	f(i,0,n) {
+		f(j,0,n) {
+			p[i][j] = i;
+		}
+	}
+
+	for (int k = 0; k < n; ++k)
+		for (int i = 0; i < n; ++i)
+			for (int j = 0; j < n; ++j)
+				if (adj[i][k] + adj[k][j] < adj[i][j]) {
+					adj[i][j] = adj[i][k]+adj[k][j];
+					p[i][j] = p[k][j];
+				}
+}
+
+vi restorePath(int u, int v) {
+
+	if (adj[u][v] == INF) return {};
+	vi path;
+	for (; v != u; v = p[u][v]) {
+		if (v == -1) return {};
+		path.push_back(v);
+	}
+	path.push_back(u);
+	reverse(path.begin(), path.end());
+	return path;
+}
 
 void floyd_warshall(int n) {
 
 	for (int k = 0; k < n; ++k)
 	for (int u = 0; u < n; ++u)
 	for (int v = 0; v < n; ++v)
-		AM[u][v] = min(AM[u][v], AM[u][k]+AM[k][v]);
-}
-
-void printAnswer(int n) {
-	for (int u = 0; u < n; ++u)
-	for (int v = 0; v < n; ++v)
-	cout << "APSP("<<u<<", "<<v<<") = " << AM[u][v] << endl;
+		adj[u][v] = min(adj[u][v], adj[u][k]+adj[k][v]);
 }
 
 void solve() {
@@ -24,17 +53,18 @@ void solve() {
 	int n, ed; cin >> n >> ed;
 	f(u,0,n) {
 		f(v,0,n) {
-			AM[u][v] = INF;
+			adj[u][v] = INF;
 		}
-		AM[u][u] = 0;
+		adj[u][u] = 0;
 	}
 
 	f(i,0,ed) {
 		int u, v, w; cin >> u >> v >> w;
-		AM[u][v] = w;
+		adj[u][v] = w;
 	}
 
 	floyd_warshall(n);
 
-	// printAnswer(n);
+	// prepareParent();
+	// vi path = restorePath(0, 3);
 }
