44266f8689eddb0b2447f57d5114ea43eab450b7
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2025-04-01 17:35:22 -0300

    BFS

--- LibNova/Codigo/Grafos/bfs - niveis.cpp
+++ LibNova/Codigo/Grafos/bfs - niveis.cpp
@@ -2,21 +2,23 @@
 // 
 //  Encontrar distancia entre S e outros pontos em que pontos estao agrupados (terminais)
 
-const int MAXN = 1e4+10;
-const int MAXEDG = 1e4+10;
+const int MAXN = 510;
+const int MAXEDG = 510; // maximo numero de terminais
 
 int dist[MAXN];
-vector<int> niveisDoNode[MAXN], itensDoNivel[MAXEDG];
+vector<int> niveisDoNode[MAXN], nodesDoNivel[MAXEDG];
 
 void bfs(int s) {
 
     queue<pair<int, int>> q; q.push({s, 0});
 
+	dist[s] = 0;
+
     while (!q.empty()) {
         auto [v, dis] = q.front(); q.pop(); 
 
         for(auto nivel : niveisDoNode[v]) {
-            for(auto u : itensDoNivel[nivel]) {
+            for(auto u : nodesDoNivel[nivel]) {
                 if (dist[u] == 0) {
                     q.push({u, dis+1});
                     dist[u] = dis + 1;
@@ -28,16 +30,18 @@ void bfs(int s) {
 
 void solve() {
 
-	int n, ed;
-	f(i,0,ed) {
-		int q; cin >> q; // quantidade de elementos nesse nivel
+	int n, terminais, s, e;
 
+	f(i,0,terminais) {
+		int q; cin >> q;
 		while(q--) {
-            int v; cin >> v;
+			int v; v--;
 			niveisDoNode[v].push_back(i);
-			itensDoNivel[i].push_back(v);
-        }
+			nodesDoNivel[i].push_back(v);
+		}
 	}
 
-	int s; bfs(s);
+	bfs(s);
+
+	cout << dist[e] << endl;
 }
